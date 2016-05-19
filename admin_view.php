75ae5d20e4e47ba5dce568bd65a8f84b2821e43d
Marcel Henderson <mhende25@db2.emich.edu>
2016-05-19 18:09:59 -0400

    added admin page

---
+++ admin_view.php
@@ -0,0 +1,48 @@
+<html>
+    <head><title>Administrator Reports</title></head>
+    <body>
+        <h1>Reports</h1>
+	<p>Total number of registered customers:  </p>
+
+	<table border="1">
+	    <tr>
+		<th colspan="2">Book Inventory Report</th>
+	    </tr>
+	    <tr>
+		<th>Category</th>
+		<th>Number of Books</th>
+	    </tr>
+        </table>
+	
+	<br/><br/>	
+	<table border = "1">
+	    <tr>
+		<th colspan="2">Sales by Month</th>
+	    </tr>
+	    <tr>
+		<th>Month</th>
+		<th>Income</th>
+	    </tr>
+	</table>
+
+	<br/><br/>
+	<table border="1">
+	    <tr>
+		<th colspan="2">Book Reviews</th>
+	    </tr>
+	    <tr>
+		<th>Book Title</th>
+		<th>Number of Reviews</th>
+	    </tr>
+	</table>
+
+	<br/><br/>
+	<input type="button" name="exit" onclick="location.href='screen1.php';" value="EXIT 3-B.com"/>
+    </body>
+</html>
+	   
+
+
+
+    <body>
+</html>
