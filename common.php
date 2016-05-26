c2588063222da296c1afc43dfc44755502ddcd56
jdavi151 <jdavi151@emich.edu>
2016-05-26 12:57:57 -0400

    Added Common.php
    Added the common file that we will include for all of our connections to
    the database.

---
+++ common.php
@@ -0,0 +1,16 @@
+<?php
+	$db_server = "localhost";
+	$db_user = "201605_471_g04";
+	$db_password = "68WM6UZ644IP";
+		
+	function db_open()	{
+		global $link, $db_server, $db_user, $db_password;
+		$link = @mysqli_connect($db_server, $db_user, $db_password)
+					or die("Could not connect: " . mysqli_connect_error());
+	}
+	
+	function db_close()	{
+		global $link;
+		@mysqli_close($link);
+	}
+?>
