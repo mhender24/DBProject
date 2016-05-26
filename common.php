e2788bead5dec0e91ee6b3d5ad2c7e2c6a7d49bd
jdavi151 <jdavi151@emich.edu>
2016-05-26 18:52:21 -0400

    Added db_name to common.php
    db_name is now a variable in common.php

--- common.php
+++ common.php
@@ -2,10 +2,11 @@
 	$db_server = "localhost";
 	$db_user = "201605_471_g04";
 	$db_password = "68WM6UZ644IP";
+	$db_name = "201605_471_g04";
 		
 	function db_open()	{
-		global $link, $db_server, $db_user, $db_password;
-		$link = @mysqli_connect($db_server, $db_user, $db_password)
+		global $link, $db_server, $db_user, $db_password, $db_name;
+		$link = @mysqli_connect($db_server, $db_user, $db_password, $db_name)
 					or die("Could not connect: " . mysqli_connect_error());
 	}
 	
