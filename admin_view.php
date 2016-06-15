072c066727e0b351df20cd4f45443ca9240e6661
System Administrator <root@Marcel-Hendersons-MacBook-Pro.local>
2016-06-14 23:32:00 -0400

    functionality for admin view

--- admin_view.php
+++ admin_view.php
@@ -1,8 +1,33 @@
+<?php
+	$cust_count_sql = "	SELECT count(*) 
+						FROM users";
+	$book_count_sql = " SELECT category, count(*)
+						FROM book
+						GROUP BY Category
+						ORDER BY count(*) DESC";
+	$month_sales_sql =" ";
+	$book_review_sql =" SELECT b.title, count(r.review_number)
+						FROM book as b, reviews as r
+						WHERE b.isbn = r.isbn 
+						GROUP BY b.title";
+
+	include 'common.php';
+	db_open();
+
+	$cust_count_result =  mysqli_query($link, $cust_count_sql);
+	$cust_row = mysqli_fetch_assoc($cust_count_result);
+
+	
+
+?>
+
+
+
 <html>
     <head><title>Administrator Reports</title></head>
     <body>
         <h1>Reports</h1>
-	<p>Total number of registered customers:  </p>
+	<p>Total number of registered customers:  <?=$cust_row['count(*)']?></p>
 
 	<table border="1">
 	    <tr>
@@ -12,6 +37,18 @@
 		<th>Category</th>
 		<th>Number of Books</th>
 	    </tr>
+	    <?php
+	    if ($book_count_result = mysqli_query($link, $book_count_sql)) {
+			while($row = mysqli_fetch_assoc($book_count_result) ){
+
+				echo "<tr><td>";
+				echo $row["category"];
+				echo "</td><td>";
+				echo $row['count(*)'];
+				echo "</td></tr>";
+			}
+		}
+		?>
         </table>
 	
 	<br/><br/>	
@@ -34,6 +71,17 @@
 		<th>Book Title</th>
 		<th>Number of Reviews</th>
 	    </tr>
+	    <?php
+	    if ($book_review_result = mysqli_query($link, $book_review_sql)) {
+			while($row = mysqli_fetch_assoc($book_review_result) ){
+				echo "<tr><td>";
+				echo $row["title"];
+				echo "</td><td>";
+				echo $row['count(r.review_number)'];
+				echo "</td></tr>";
+			}
+		}
+		?>
 	</table>
 
 	<br/><br/>
