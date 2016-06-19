756b759c1ce3b8a73f7ae92ae42c1a1d22a4910d
jdavi151 <jdavi151@emich.edu>
2016-06-19 13:16:55 -0400

    Revert "unset cart on confirm order"
    This reverts commit 934aa5c0987a8e8d0be861c591d8b71bf3971ffc.

--- confirm_order.php
+++ confirm_order.php
@@ -44,8 +44,6 @@
 								VALUES('" . $randid . "', '" . $_SESSION['cart'][$i] . "', " . $_SESSION['quantities'][$i] . ");";
 				mysqli_query($link, $content_sql);
 			}
-			unset($_SESSION['cart']);
-			unset($_SESSION['quantities']);
 			header("Location: proof_purchase.php");
 		}
 	}
